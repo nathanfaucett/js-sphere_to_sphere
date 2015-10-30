4ce80bc03a0b3e0a8e08d14326fa28cf83585b91
nathanfaucett <nathanfaucett@gmail.com>
2015-10-30 17:03:54 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -1,19 +1,12 @@
 var tape = require("tape"),
-    sphereToSphere = require("../src/index");
+    vec3 = require("vec3"),
+    sphereToSphere = require("..");
 
 
 tape("sphereToSphere(x1 : Number, y1 : Number, z1 : Number, r1 : Number, x2 : Number, y2 : Number, z2 : Number, r2 : Number)", function(assert) {
     assert.deepEqual(sphereToSphere(-1, 0, 0, 1.1, 1, 0, 0, 1.1), {
-        normal: {
-            0: 1,
-            1: 0,
-            2: 0
-        },
-        point: {
-            0: 0,
-            1: 0,
-            2: 0
-        },
+        normal: vec3.create(1, 0, 0),
+        point: vec3.create(0, 0, 0),
         depth: 0.10000000000000009,
         data: null
     });
